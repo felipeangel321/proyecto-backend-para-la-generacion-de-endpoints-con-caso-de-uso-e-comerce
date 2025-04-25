a2f7bc691435822a60d67d9e0f2034a629dbacbe
Felipeangel321 <felipeangel90@gmail.com>
2025-04-25 12:31:06 -0400

    asdas

--- src/main/java/com/felipeangel/blablapirest/restapi/RestapiApplication.java
+++ src/main/java/com/felipeangel/blablapirest/restapi/RestapiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
